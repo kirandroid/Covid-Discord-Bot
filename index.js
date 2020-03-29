1acbee3247442a7e8925e367b4ca3f96cdad1416
Kiran Pradhan <pradhanangakiran@gmail.com>
2020-03-29 14:16:41 +0545

    Added message reply markup

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const Text = require("discord-markup");
 const axios = require("axios");
 const Discord = require("discord.js");
 const client = new Discord.Client();
@@ -19,17 +20,23 @@ client.on("message", msg => {
         console.log(error);
       });
   } else if (command == "help") {
-    msg.reply(`\n-----------Command List-----------\n
-      #1 -global_stat\n
-      #2 -total_stat <Country-Code>: total_stat NP\n
-      #3 -total_cases <Country-Code>\n
-      #4 -total_recovered <Country-Code>\n
-      #5 -total_unresolved <Country-Code>\n
-      #6 -total_deaths <Country-Code>\n
-      #7 -total_new_cases_today <Country-Code>\n
-      #8 -total_new_deaths_today <Country-Code>\n
-      #9 -total_active_cases <Country-Code>\n
-      #10 -total_serious_cases <Country-Code>`);
+    msg.reply(
+      String(
+        new Text(
+          `\n-----------Command List-----------\n
+#1 -global_stat\n
+#2 -total_stat <Country-Code>: total_stat NP\n
+#3 -total_cases <Country-Code>\n
+#4 -total_recovered <Country-Code>\n
+#5 -total_unresolved <Country-Code>\n
+#6 -total_deaths <Country-Code>\n
+#7 -total_new_cases_today <Country-Code>\n
+#8 -total_new_deaths_today <Country-Code>\n
+#9 -total_active_cases <Country-Code>\n
+#10 -total_serious_cases <Country-Code>`
+        ).codeblock()
+      )
+    );
   } else {
     axios
       .get(`https://thevirustracker.com/free-api?countryTotal=${args}`)
@@ -48,7 +55,13 @@ client.on("message", msg => {
 
 function printAll(msg, response, isGlobal) {
   return msg.reply(
-    `${!isGlobal ? `Country : ${response.info.title}\n` : "Global Stats"}
+    String(
+      new Text(
+        `${
+          !isGlobal
+            ? `Country : ${response.info.title}\n`
+            : "---------Global Stats---------"
+        }
     Total Cases : ${response.total_cases}\n
     Total Recovered : ${response.total_recovered}\n
     Total Unresolved : ${response.total_unresolved}\n
@@ -57,17 +70,23 @@ function printAll(msg, response, isGlobal) {
     Total New Deaths Today : ${response.total_new_deaths_today}\n
     Total Active Cases : ${response.total_active_cases}\n
     Total Serious Cases : ${response.total_serious_cases}`
+      ).codeblock()
+    )
   );
 }
 
 function printResult(msg, command, response) {
   return msg.reply(
-    `${command
-      .replace(/_/g, " ")
-      .toLowerCase()
-      .split(" ")
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(" ")} in ${response.info.title} is ${response[command]}`
+    String(
+      new Text(
+        `${command
+          .replace(/_/g, " ")
+          .toLowerCase()
+          .split(" ")
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")} in ${response.info.title} is ${response[command]}`
+      ).codeblock()
+    )
   );
 }
 client.login(process.env.BOT_TOKEN);
